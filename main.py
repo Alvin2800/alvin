dfe335981766d3a69ec8c541169664b181085559
Alvin2800 <139653846+Alvin2800@users.noreply.github.com>
2026-03-16 21:59:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import os
 
 app = Flask(__name__)
 
@@ -7,15 +6,18 @@ temperature = 0
 
 @app.route("/")
 def home():
+    print("Route / appelée", flush=True)
     return "ESP32 IoT Server Running", 200
 
 @app.route("/data")
 def data():
     global temperature
+    print("Route /data appelée", flush=True)
     temperature = request.args.get("temp", 0)
     print("Température reçue :", temperature, flush=True)
     return "OK", 200
 
 @app.route("/temperature")
 def get_temperature():
+    print("Route /temperature appelée", flush=True)
     return jsonify({"temperature": temperature})
